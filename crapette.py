db74d7f0f2833ee94534d743b7a35872134b86d3
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2020-06-11 04:16:01 +0200

    Use layouts for positions

--- crapette.py
+++ crapette.py
@@ -3,10 +3,35 @@ import kivy
 kivy.require("1.11.0")
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
 
 
 class BackGround(FloatLayout):
     pass
+
+
+class FoundationWidget(RelativeLayout):
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            print("fufu", touch)
+
+
+class TableauWidgetLeft(RelativeLayout):
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            print("tata left", touch)
+
+
+class TableauWidgetRight(RelativeLayout):
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            print("tata right", touch)
+
+
+class PlayerPileWidget(RelativeLayout):
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            print("player", touch)
 
 
 # main app
