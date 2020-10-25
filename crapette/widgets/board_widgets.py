b8ca3c7b6cca77150e24b5301ba798ce9d7e6ecb
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2020-10-25 02:28:03 +0100

    Modularity, first step to drop on other pile

--- crapette/widgets/board_widgets.py
+++ crapette/widgets/board_widgets.py
@@ -1,7 +1,4 @@
-from kivy.uix.scatterlayout import ScatterLayout
-from kivy.properties import StringProperty, BooleanProperty, NumericProperty
-
-from ..images.card_deck import card2img, CARD_IMG
+from .card_widget import CardWidget
 
 
 class BoardWidget:
@@ -18,7 +15,7 @@ class BoardWidget:
             # print(pile_widget.pile.name)
             for index, card in enumerate(pile_widget.pile):
                 # print(pile_widget.card_pos(index), card)
-                card_widget = MovingCard(card, self.app)
+                card_widget = CardWidget(card, self.app)
                 card_widget.set_center_pos(*pile_widget.card_pos(index))
                 card_widget.do_translation = False
                 self.card_widgets[card] = card_widget
@@ -32,51 +29,3 @@ class BoardWidget:
             if pile:
                 print(pile.can_pop_card(player))
                 self.card_widgets[pile[-1]].do_translation = pile.can_pop_card(player)
-
-
-class MovingCard(ScatterLayout):
-    source = StringProperty()
-
-    def __init__(self, card, app):
-        super().__init__()
-        self._card = card
-        self._app = app
-        self.source = card2img(card)
-
-    def set_center_pos(self, x, y):
-        self.pos = (x - self._app.card_width / 2, y - self._app.card_height / 2)
-
-    def on_touch_down(self, touch):
-        if not self.collide_point(touch.x, touch.y):
-            return
-
-        if touch.is_double_tap:
-            print("DOUBLE TOUCH DOWN", self._card)
-            return True
-
-        print("TOUCH DOWN", self._card)
-
-        self._last_pos = self.pos
-
-        return super().on_touch_down(touch)
-
-    def on_touch_up(self, touch):
-        super().on_touch_up(touch)
-        if touch.grab_current is not None:
-            return
-        if not self.collide_point(touch.x, touch.y):
-            return
-
-        # Look for the pile the card was dropped
-        for pile in self._app.pile_widgets:
-            if pile.collide_point(touch.x, touch.y):
-                print(pile.pile.name)
-                break
-        else:
-            print("Not dropped on a pile")
-            if self._last_pos:  # Just in case...
-                self.pos = self._last_pos
-        self._last_pos = None
-
-        print("TOUCH UP", touch.grab_current, self._card)
-        return False
