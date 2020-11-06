3bf3e4c9fc74795e22e390693c179075c8b47665
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2020-11-06 03:14:53 +0100

    Scores to optimize next move

--- crapette/core/brainforce.py
+++ crapette/core/brainforce.py
@@ -1,5 +1,5 @@
 # importing the sys module
-from crapette.core.piles import TableauPile
+from crapette.core.piles import TableauPile, _PlayerPile
 import sys
 
 # the setrecursionlimit function is
@@ -9,6 +9,8 @@ import sys
 # to satisfy our needs
 
 sys.setrecursionlimit(10 ** 5)
+
+from pprint import pprint
 
 from .board import Board, Move
 
@@ -28,8 +30,31 @@ class BrainForce:
     def compute_states(self):
         debug("*" * 50)
         debug(f"compute_states for player {self.player}")
-        # Start recursive state modifications
-        self._recurse_states({}, self.board, [], "")
+        # Recursive state modifications
+        states = {}
+        self._recurse_states(states, self.board, [], "")
+        print("states")
+        pprint(states)
+
+        # Score for each state
+        scores = {b: BoardScore(b, self.player).score for b in states}
+        print("scores")
+        pprint(scores)
+        max_score = max(scores.values())
+        print("max score", max_score)
+
+        # Boards with max score
+        max_boards = [b for b, s in scores.items() if s == max_score]
+        print("max boards")
+        pprint(max_boards)
+
+        # Boards with max score and less moves
+        max_moves = [states[b] for b in max_boards]
+        print("max moves")
+        pprint(max_moves)
+        nb_min_moves = min(len(m) for m in max_moves)
+        print("max moves")
+        pprint([m for m in max_moves if len(m) == nb_min_moves])
 
     def _recurse_states(self, states: dict, board: Board, moves: list, alinea):
         debug(f"{alinea}recurse_states: {len(states)} states, {len(moves)} moves")
@@ -47,44 +72,42 @@ class BrainForce:
         # Register this new board
         debug(f"{alinea}  register board")
         states[board] = moves
-        # for b in states:
-        #     print("-", hash(b))
+
+        # Player move stops the recursion
+        if moves and isinstance(moves[-1].origin, _PlayerPile):
+            return
 
         # Try possible moves fom this board
-        for pile_orig in board.piles:
+        for pile_orig in board.tableau_piles + list(board.players_piles[self.player]):
             if pile_orig.is_empty:
-                # print(f"{alinea}  {pile_orig.name} is empty")
                 continue
             if not pile_orig.can_pop_card(self.player):
-                # print(f"{alinea}  {pile_orig.name} not poppable")
                 continue
             if not pile_orig.face_up:
-                # print(f"{alinea}  {pile_orig.name} not flipped")
                 continue
 
             card = pile_orig.top_card
-            # print(f"{alinea}  take {card} from {pile_orig.name}")
 
             to_empty_tableau_before = False
             for pile_dest in board.piles:
                 if (
                     pile_dest == pile_orig
-                    or pile_dest.name == f"WastePlayer{self.player}"
+                    or pile_dest.name
+                    == f"WastePlayer{self.player}"  # Could be useless depending on the rest of the algorithm
                 ):
                     continue
                 if pile_dest.can_add_card(card, pile_orig, self.player):
                     if pile_dest.is_empty:
-                        if isinstance(pile_dest, TableauPile):
+                        if isinstance(pile_orig, TableauPile) and isinstance(
+                            pile_dest, TableauPile
+                        ):
                             # Avoid trying each empty slot, it's useless
                             if to_empty_tableau_before:
                                 print(
                                     f"{alinea}    skip move {card} from {pile_orig.name} to empty {pile_dest.name}: move to empty already done"
                                 )
                                 continue
-                            elif (
-                                isinstance(pile_orig, TableauPile)
-                                and len(pile_orig) == 1
-                            ):
+                            elif len(pile_orig) == 1:
                                 print(
                                     f"{alinea}    skip move {card} from {pile_orig.name} to empty {pile_dest.name}: would just swap empty slots"
                                 )
@@ -112,3 +135,39 @@ class BrainForce:
 
                     # Recurse the hell out of it
                     self._recurse_states(states, next_board, next_moves, "_" + alinea)
+
+
+class BoardScore:
+    WORSE = (float("inf"),) * 11
+
+    def __init__(self, board: Board, player: int):
+        self.board = board
+        self.player = player
+
+    @property
+    def score(self):
+        return (
+            self.foundation_score,
+            self.crapette_score,
+            self.empty_tableau_score,
+        ) + tuple(self.clean_tableau_score)
+
+    @property
+    def foundation_score(self):
+        return sum(len(pile) for pile in self.board.foundation_piles)
+
+    @property
+    def crapette_score(self):
+        return -len(self.board.players_piles[self.player].crape)
+
+    @property
+    def empty_tableau_score(self):
+        empty_slot = 0
+        for pile in self.board.tableau_piles:
+            if pile.is_empty:
+                empty_slot += 1
+        return empty_slot
+
+    @property
+    def clean_tableau_score(self):
+        return sorted((len(pile) for pile in self.board.tableau_piles), reverse=True)
