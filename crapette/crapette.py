d207596f93b162f384f7a66d313402e69589dffe
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2020-11-10 01:07:24 +0100

    Beautiful menu on startup

--- crapette/crapette.py
+++ crapette/crapette.py
@@ -12,12 +12,12 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.properties import BooleanProperty, NumericProperty
 from kivy.core.window import Window
-
+import kivy.config
 import kivy.resources
 
 kivy.require("1.10.0")
 kivy.resources.resource_add_path(str(Path(__file__).parent))
-print(str(Path(__file__).parent / "crapette"))
+kivy.config.Config.set("input", "mouse", "mouse,multitouch_on_demand")
 
 from .images.card_deck import CARD_IMG
 from .board_manager import BoardManager
@@ -53,9 +53,6 @@ class CrapetteApp(App):
         self.on_window_resize(Window, *Window.size)
         Window.bind(on_resize=self.on_window_resize)
 
-    def new_game(self):
-        self.board_manager.new_game()
-
     def on_window_resize(self, window, width, height):
         self.card_height = height / Board.NB_ROWS
         self.card_width = self.card_height * CARD_IMG.RATIO
@@ -70,6 +67,21 @@ class CrapetteApp(App):
         self.wide = width / height > game_width_max / game_height
 
         self.board_manager.place_cards()
+
+    def set_menu_visible(self, visible):
+        ids = self.root.ids
+
+        menu = ids["menu"]
+        menu.opacity = 1 if visible else 0
+        menu.disabled = False if visible else True
+
+        menu_button = ids["menu_button"]
+        menu_button.opacity = 0 if visible else 1
+        menu_button.disabled = True if visible else False
+
+    def new_game(self):
+        self.set_menu_visible(False)
+        self.board_manager.new_game()
 
 
 def main():
