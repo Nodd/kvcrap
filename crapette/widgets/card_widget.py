7b2b890a806a1744e6a7e1cb54ef380072c28c21
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2023-06-08 00:30:55 +0200

    CardWidget Documentation

--- crapette/widgets/card_widget.py
+++ crapette/widgets/card_widget.py
@@ -21,77 +21,149 @@ def debug(*s):
 
 
 class CardWidget(ScatterLayout):
+    """Widget representing a card on the board"""
+
     source = StringProperty()
 
     def __init__(self, card, game_manager):
-        super().__init__()
         self.card = card
-        self.game_manager = game_manager
         self.source = card2img(card)
+        self.game_manager = game_manager
 
         self.pile_widget = None
         self._moving = False
         self._flipping = False
         self.main_rotation = 0
 
+        # At the end so that repr() works in the kivy initalization
+        super().__init__()
+
     def __repr__(self):
-        try:
-            return f"CardWidget({self.card!r})"
-        except AttributeError:
-            return "CardWidget()"
+        return f"CardWidget({self.card!r})"
 
     def set_center_animated(self, pos):
+        """Animate a card move on the board.
+
+        `pos` is the new coordinates of the car center.
+
+        This is only a visual effect, this function doesn't change
+        the state of the game.
+        """
         animation = Animation(center=pos, duration=0.1, transition="out_quad")
         animation.start(self)
 
     def update_image(self):
+        """Shows the correct image for the card.
+
+        It can be the face or the back, depending on the card state.
+
+        This is only a visual effect, this function doesn't change
+        the state of the game.
+        """
         self.source = card2img(self.card)
 
-    def _random_rotation_animation(self):
+    def random_rotation_animation_factory(self, duration=0.1):
+        """Create an Animation to display the card with a random rotation error.
+
+        It's just for fun, to make it look like the card was placed by hand on the board,
+        not perfectly aligned.
+
+        This is only a visual effect, this function doesn't change
+        the state of the game.
+        """
         angle = self.main_rotation
         angle += random.triangular(-MAX_RANDOM_ANGLE, MAX_RANDOM_ANGLE)
         if self.rotation > 180:
             angle += 360
-        return Animation(rotation=angle, duration=0.1, transition="out_sine")
+        return Animation(rotation=angle, duration=duration, transition="out_sine")
 
     def apply_random_rotation(self, main_rotation: float = None):
+        """Apply a random rotation error.
+
+        Optionally change the global rotation of the card, for example on the foundation piles.
+
+        This is only a visual effect, this function doesn't change
+        the state of the game.
+        """
         if main_rotation is not None:
             self.main_rotation = main_rotation
-        self._random_rotation_animation().start(self)
+        self.random_rotation_animation_factory().start(self)
 
     def set_face_up(self):
+        """Flip the card so that the face is up.
+
+        This changes the underlying card model accordingly.
+        """
         self.card.face_up = True
         self.flip_animation()
 
     def set_face_down(self):
+        """Flip the card so that the face is down.
+
+        This changes the underlying card model accordingly.
+        """
         self.card.face_up = False
         self.flip_animation()
 
     def flip_animation(self):
+        """Animate a card flip.
+
+        This is only a visual effect, this function doesn't change
+        the state of the game.
+        """
+        # Save state
         height = self.height
-        animation = Animation(height=0, duration=0.25, transition="out_sine")
+        y = self.y
+
+        # Fade out by reducing the height and compensing the position so that the center doesn't change
+        duration_out = 0.2
+        animation = Animation(height=0, duration=duration_out, transition="out_sine")
+        animation &= Animation(
+            y=y + height / 2, duration=duration_out, transition="out_sine"
+        )
+
+        # Update the image after the first animation
         animation.on_complete = lambda *args: self.update_image()
+
+        # Fade in by restoring the height and the position, while adding a small rotation error
+        duration_in = 0.1
         animation += (
-            Animation(height=height, duration=0.1, transition="in_sine")
-            & self._random_rotation_animation()
+            Animation(height=height, duration=duration_in, transition="in_sine")
+            & Animation(y=y, duration=duration_in, transition="in_sine")
+            & self.random_rotation_animation_factory()
         )
         animation.start(self)
 
     @property
     def is_top(self):
+        """Check if the card is on the top of its pile.
+
+        This is only an information return, this property doesn't change
+        the state of the game.
+        """
+        # TODO: avoid using a reference to the pile widget, but how ?
         return self.card == self.pile_widget.pile.top_card
 
     def on_touch_down(self, touch):
+        """Starts a movement when the card is pressed down, if the current player has the right to do it.
+
+        Return `False` to continue event propagation, or `True` to stop it.
+        """
+        # All objects receive all events, manage only when the touch event is on the card
         if not self.collide_point(touch.x, touch.y):
             return False
 
+        # If there are other cards on top, the player can do nothing with it.
         if not self.is_top:
             return False
 
+        # Stop the event if this is the end of the game.
         if self.game_manager.active_player is None:
             debug("End of game")
             return True
 
+        # Check if the card can be moved by the player
+        # TODO: avoid using self.pile_widget and self.game_manager, how ?
         debug("TOUCH DOWN", self.card)
         can_pop = self.pile_widget.pile.can_pop_card(self.game_manager.active_player)
         assert can_pop in (True, False), can_pop
